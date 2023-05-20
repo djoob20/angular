de1626ebe9842b376430f33ad0b079266b11c984
djoob20 <djoob20@gmail.com>
2023-05-20 22:18:30 +0200

    added app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { Component, NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+
+const routes: Routes = [
+  {path:'facesnaps', component: FaceSnapListComponent}
+]
+@NgModule({
+  imports:[
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule{
+
+}
