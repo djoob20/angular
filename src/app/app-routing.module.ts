83dbbbbbddba1b83751a2971a20d9eb68dda9371
djoob20 <djoob20@gmail.com>
2023-05-20 22:57:11 +0200

    added routerLink and navigated into routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
-import { Component, NgModule } from "@angular/core";
+import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 const routes: Routes = [
-  {path:'facesnaps', component: FaceSnapListComponent}
+  {path:'facesnaps', component: FaceSnapListComponent},
+  {path:'', component: LandingPageComponent}
 ]
 @NgModule({
   imports:[
@@ -13,6 +15,7 @@ const routes: Routes = [
     RouterModule
   ]
 })
+
 export class AppRoutingModule{
 
 }
