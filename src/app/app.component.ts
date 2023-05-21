ae11b276d7ccc8854488fee448de63b80b94c84b
djoob20 <djoob20@gmail.com>
2023-05-21 21:07:07 +0200

    tested observable: mergeMap, concatMap, exhaustMap, switchMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {filter, interval, map, Observable, tap} from "rxjs";
+import {concatMap, delay, filter, interval, map, mergeMap, Observable, of, take, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,17 @@ import {filter, interval, map, Observable, tap} from "rxjs";
 export class AppComponent implements OnInit{
 
   interval$ !:Observable<string>
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
   ngOnInit(): void {
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      concatMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+    /*
     this.interval$ = interval(1000).pipe(
       filter(value => value % 3 ===0),
       map(value => value % 2 === 0 ?
@@ -19,10 +29,26 @@ export class AppComponent implements OnInit{
       //give the text generated for map(...)
       tap(text => this.logger(text))
     );
+
+     */
   }
 
   logger(text: string):void{
     console.log(`Log: ${text}`);
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
+    );
+  }
+
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
   }
 
 }
