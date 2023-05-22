04a0e11aaf6013b1767cf6cd0fda406d5e160fc7
djoob20 <djoob20@gmail.com>
2023-05-22 16:14:24 +0200

    unsubscribe to observable by implemeting onDestry()

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.models';
 import { FaceSnapsService } from '../services/face-snaps.services';
-import { interval, take, tap } from 'rxjs';
+import { Subject, interval, take, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent implements OnInit{
+export class FaceSnapListComponent implements OnInit, OnDestroy{
 
 
   faceSnaps!: FaceSnap[];
+  destroy$!: Subject<boolean>;
 
   constructor(private faceSnapsServie: FaceSnapsService){}
 
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+  }
+
   ngOnInit(): void {
     this.faceSnaps = this.faceSnapsServie.getAllFaceSnaps();
+    this.destroy$ = new Subject<boolean>;
 
     interval(1000).pipe(
-      take(1),
+      takeUntil(this.destroy$),
       tap(console.log)
     ).subscribe();
   }
